bc41b9cd74893f7c5d9c6e1662500c1dc4679659
Eduardo de Albuquerque <erizzoalbuquerque@gmail.com>
2020-06-05 16:30:50 -0300

    Created dialogue and curse

---
+++ UnityProject/Assets/FaceTarget.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceTarget: MonoBehaviour
+{
+    public Transform _target;
+    public Transform _upReference;
+
+    public float smooth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion rotation = Quaternion.LookRotation(_target.position - this.transform.position, _upReference.up);
+        this.transform.rotation = Quaternion.Lerp(rotation, this.transform.rotation, smooth);
+    }
+}
